fb3d80524bc5f6e478dce3ed6b76a34b9fa5e4b3
Wasim Rehman <100414282+WasimRehmn@users.noreply.github.com>
2022-03-04 20:01:56 +0530

    I added js functionality part on homepage

--- scripts/home.js
+++ scripts/home.js
@@ -1,40 +1,58 @@
 localStorage.setItem("data_i", JSON.stringify([]));
 localStorage.setItem("data_j", JSON.stringify([]));
 localStorage.setItem("data_k", JSON.stringify([]));
-let sliding_img = [
-    {
-        image: "https://logan.nnnow.com/content/dam/nnnow-project/18-feb-2022/ABH_TopbannerDesktop_gif.gif",
-        text: "ABH TopbannerDesktop gif",
-        id: "000",
-    },
-    {
-        image: "https://logan.nnnow.com/content/dam/nnnow-project/10-feb-2022/SmashboxTopbanner-DesktopGIF.gif",
-        text: "SmashboxTopbanner DesktopGIF",
-        id: "001",
-    },
-    {
-        image: "https://logan.nnnow.com/content/dam/nnnow-project/24-feb-2022/Benefit_TopbannerDesktop.jpg",
-        text: "Benefit TopbannerDesktop",
-        id: "002",
-    },
-    {
-        image: "https://logan.nnnow.com/content/dam/nnnow-project/2-mar-2022/Lancome_TopBannerDesktop_Get7mlGenefique.jpg",
-        text: "Lancome TopBannerDesktop Get7mlGenefique",
-        id: "003",
-    },
-    {
-        image: "https://logan.nnnow.com/content/dam/nnnow-project/2-mar-2022/Sephora_ContentTopBanner_Tresses'n'TextureDesktop.jpg",
-        text: "Sephora ContentTopBanner Tresses'n'TextureDesktop",
-        id: "004",
-    },
-];
+
+let i = 0;
+let j = 0;
+let k = 0;
+
+let currentSlide = 0;
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
+
+const init = (n) => {
+    slides.forEach((slide, index) => {
+        slide.style.display = "none";
+        dots.forEach((dot, index) => {
+            dot.classList.remove("active");
+        });
+    });
+    slides[n].style.display = "block";
+    dots[n].classList.add("active");
+};
+document.addEventListener("DOMContentLoaded", init(currentSlide));
+const next = () => {
+    currentSlide >= slides.length - 1 ? (currentSlide = 0) : currentSlide++;
+    init(currentSlide);
+};
+
+const prev = () => {
+    currentSlide <= 0 ? (currentSlide = slides.length - 1) : currentSlide--;
+    init(currentSlide);
+};
+
+document.getElementById("next").addEventListener("click", next);
+
+document.getElementById("prev").addEventListener("click", prev);
+
+setInterval(() => {
+    next();
+}, 3000);
+
+dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => {
+        console.log(currentSlide);
+        init(i);
+        currentSlide = i;
+    });
+});
 
 const showData_i = (i) => {
     // console.log(data, i);
     let prodBox = document.getElementById("slide1");
     prodBox.textContent = "";
     let data = JSON.parse(localStorage.getItem("data_i"));
-    console.log(data);
+    // console.log(data);
     for (let y = 0; y < 6; y++) {
         let card = document.createElement("div");
         card.setAttribute("class", "prod");
@@ -66,7 +84,7 @@ const showData_j = (j) => {
     let prodBox = document.getElementById("slide2");
     prodBox.textContent = "";
     let data = JSON.parse(localStorage.getItem("data_j"));
-    console.log(data);
+    // console.log(data);
     for (let y = 0; y < 3; y++) {
         let card = document.createElement("div");
         card.setAttribute("class", "prod1");
@@ -98,7 +116,7 @@ const showData_k = (k) => {
     let prodBox = document.getElementById("slide3");
     prodBox.textContent = "";
     let data = JSON.parse(localStorage.getItem("data_k"));
-    console.log(data);
+    // console.log(data);
     for (let y = 0; y < 6; y++) {
         let card = document.createElement("div");
         card.setAttribute("class", "prod1");
@@ -125,18 +143,6 @@ const showData_k = (k) => {
     }
 };
 
-let main_sliding = document.getElementById("img");
-let x = 0;
-setInterval(function () {
-    if (x === sliding_img.length) {
-        x = 0;
-    }
-    main_sliding.src = sliding_img[x].image;
-    main_sliding.alt = sliding_img[x].text;
-    main_sliding.id = sliding_img[x].id;
-    x++;
-}, 3000);
-
 const getData = async () => {
     let res = await fetch("./data/home_data.json");
     let data = await res.json();
@@ -145,8 +151,33 @@ const getData = async () => {
     localStorage.setItem("data_j", JSON.stringify(data.JL));
     localStorage.setItem("data_k", JSON.stringify(data.EOS));
 
-    showData_i(0);
-    showData_j(0);
-    showData_k(0);
+    showData_i(i);
+    showData_j(j);
+    showData_k(k);
 };
 getData();
+const forwrd3 = () => {
+    if (k < 2) showData_k(++k);
+};
+const forwrd2 = () => {
+    if (j < 2) showData_j(++j);
+};
+const forwrd1 = () => {
+    if (i < 2) showData_i(++i);
+};
+const bak3 = () => {
+    if (k > 0) showData_k(--k);
+};
+const bak2 = () => {
+    if (j > 0) showData_j(--j);
+};
+const bak1 = () => {
+    if (i > 0) showData_i(--i);
+};
+
+document.getElementById("forwrd3").addEventListener("click", forwrd3);
+document.getElementById("back3").addEventListener("click", bak3);
+document.getElementById("forwrd2").addEventListener("click", forwrd2);
+document.getElementById("back2").addEventListener("click", bak2);
+document.getElementById("forwrd1").addEventListener("click", forwrd1);
+document.getElementById("back1").addEventListener("click", bak1);
